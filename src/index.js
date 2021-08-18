6f29cd0e55e9591e2e46277645c6757eb51a9a91
unknown <sholomitkyR@yandex.by>
2021-08-18 12:55:18 +0300

    feat: beta ver

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+const BRACKETS_PAIR = {
+  [')']: '(',
+  ['}']: '{',
+  [']']: '['
 }
+const OPEN_BRACKETS = [ '(', '{', '[']
+
+
+module.exports = function check(str, bracketsConfig) {
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    
+    if (OPEN_BRACKETS.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+
+      if (BRACKETS_PAIR[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
